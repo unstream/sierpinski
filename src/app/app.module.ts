9dbeb7561340a3f0abe63f799f9db05842944578
Ingo Weichsel <Ingo.Weichsel@adesso.de>
2017-09-26 23:29:40 +0200

    1st version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { Sierpinsky } from './sierpinsky/sierpinsky.component';
+import { Sierpinski } from './sierpinski/sierpinski.component';
 
 @NgModule({
   declarations: [
-    Sierpinsky
+    Sierpinski
   ],
   imports: [
     BrowserModule, FormsModule
   ],
   providers: [],
 //  bootstrap: [AppComponent, CanvasComponent]
-  bootstrap: [Sierpinsky]
+  bootstrap: [Sierpinski]
 })
 export class AppModule { }
