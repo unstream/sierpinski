4b4d22d92160ad419c3765ce1ce89ffa09dbcd57
unstream <unstream@web.de>
2017-09-29 09:24:11 +0200

    Not working yet

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,10 @@ describe('fractal-ui App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('Test if the page starts with 3 iterations.', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+
+
+    expect(page.getIterationInput().getAttribute('value')).toEqual('3');
   });
 });
